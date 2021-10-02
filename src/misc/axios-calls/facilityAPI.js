e1735c3b4f03f815528fd2f3a84ab946b54de730
agni1984 <agni1984@gmail.com>
2021-10-03 00:28:42 +0530

    fixes

--- src/misc/axios-calls/facilityAPI.js
+++ src/misc/axios-calls/facilityAPI.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export default axios.create({
-  //baseURL: 'http://localhost:4008/api'
-   baseURL: 'https://residentsconnect-stg.srscloudapps.link/api'
+  baseURL: 'http://localhost:4008/api'
+  // baseURL: 'https://residentsconnect-stg.srscloudapps.link/api'
 });
