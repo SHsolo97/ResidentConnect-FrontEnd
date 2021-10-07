cd0a42fd202bda33a6b31f18b8d706a75b907a99
agni1984 <agni1984@gmail.com>
2021-10-08 00:17:58 +0530

    reverted to localhost

--- src/misc/axios-calls/paymentsAPI.js
+++ src/misc/axios-calls/paymentsAPI.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export default axios.create({
- // baseURL: 'http://localhost:4012/api'
+  baseURL: 'http://localhost:4012/api'
   //baseURL: 'https://716504ba-default-ingressrc-d980-548470991.ap-south-1.elb.amazonaws.com/api'
-  baseURL: 'https://residentsconnect-stg.srscloudapps.link/api'
+  //baseURL: 'https://residentsconnect-stg.srscloudapps.link/api'
 });
