621f78f2715b945e822f56e2925dc54207b871c9
agni1984 <agni1984@gmail.com>
2021-10-07 19:17:55 +0530

    change in uri

--- src/misc/axios-calls/eventAPI.js
+++ src/misc/axios-calls/eventAPI.js
@@ -1,5 +1,7 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: 'http://localhost:4003/api/'
+  //baseURL: 'http://localhost:4003/api/'
+  baseURL: 'https://residentsconnect-stg.srscloudapps.link/api'
+
 });
