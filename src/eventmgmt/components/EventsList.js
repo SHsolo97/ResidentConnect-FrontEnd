16afb1585f21d994ee6ef32173e5f3be466a9aef
SHsolo97 <suhailharoon500@gmail.com>
2021-10-07 15:16:31 +0530

    Carousal added to EM

--- src/eventmgmt/components/EventsList.js
+++ src/eventmgmt/components/EventsList.js
@@ -4,6 +4,8 @@ import { useHistory } from 'react-router-dom';
 import EventCard from "./EventCard.js"
 import axios from "axios";
 import eventAPI from '../../misc/axios-calls/eventAPI';
+import Carousal from 'react-material-ui-carousel'
+import {Button} from '@material-ui/core';
 
 const EventsList= ({...props}) =>{
   const [events,setEvents]=useState([]);
@@ -49,15 +51,37 @@ const EventsList= ({...props}) =>{
      // eslint-disable-next-line no-use-before-define   
 
 },[]);
+var n=3;
+if(events.length<3){
+  n=events.length;
+}
+const items=[];
+for (let i = 0; i < events.length; i += n) {
+  if (i % n === 0) {
+    items.push(
+      events.slice(i,i+n).map((da,index)=>{
+        return (<div className="eventList" style={{ display: "inline-block"  }}><EventCard key={index.toString()} eventDetails={da} /></div>);
+      })
+    )
+  }
+}
 return(
         <section className="localEvents">
           <h2 style={{ color:"#3f51b5" }} >{props.heading} {props.cname} </h2>
           <div className="eventList" style={{ display: "flex", flexDirection: "row",   }}>
-          { events.map(event=>(
-                   <div>
-                         <EventCard eventDetails={event} />
-                   </div>
-            ))}
+          <Carousal NavButton={({onClick, className, style, next, prev}) => {
+        // Other logic
+
+        return (
+            <Button onClick={onClick} className={className} style={style}>
+                {next && "Next"}
+                {prev && "Previous"}
+            </Button>
+        )
+    }} 
+> 
+          {items}
+            </Carousal>
           </div>
         </section>
     );
